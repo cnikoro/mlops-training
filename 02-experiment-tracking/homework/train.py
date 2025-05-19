d04663848366afdd5c6d5cb7282e8eac82502db9
Charles Nikoro <charlesxavia@gmail.com>
2025-05-19 09:05:13 +0000

    Added hypo.py script.

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -5,9 +5,13 @@ import click
 import mlflow
 from mlflow.models import infer_signature
 
+import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+#mlflow.set_tracking_uri("sqlite:///backend.db")
+mlflow.set_training_url("http://127.0.0.1:5000")
+mlflow.set_experiment("nyc_taxi_experiment")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -22,21 +26,19 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
-
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
-
-
-if __name__ == '__main__':
-    mlflow.set_tracking_uri("sqlite:///backend.db")
-    mlflow.set_experiment("nyc_taxi_experiment")
-
     mlflow.autolog()
 
     with mlflow.start_run():
-        run_train()
+
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred))
+
+
+if __name__ == '__main__':
+    run_train()
